019980c4c50b537f919d2cc15e915ff03f65fda1
pabroux <pierre-alexandrebroux@live.fr>
2025-03-21 20:38:35 +0100

    test: add tests for utilities that can be used by any app.

---
+++ apps/shared/tests/test_utils.py
@@ -0,0 +1,32 @@
+"""Tests for the utility functions of the app.
+
+This module contains tests for helper functions that can be used by any app.
+"""
+
+from apps.shared.utils import get_formatted_content
+from stargazer import settings
+
+
+def test_get_formatted_content() -> None:
+    """Tests the `get_formatted_content` function.
+
+    Ensures that the function correctly formats the response content
+    with given message, status, and detail. Also checks for the presence
+    of the documentation URL path based on the `DOC_ACTIVATE` environment
+    variable.
+    """
+
+    message = "Test message"
+    status = 200
+    detail = ["Test detail"]
+    formatted_content = get_formatted_content(
+        message=message, status=status, detail=detail
+    )
+    assert formatted_content["message"] == message
+    assert formatted_content["status"] == status
+    assert formatted_content["detail"] == detail
+    if settings.DOC_ACTIVATE:
+        assert "documentation_url_path" in formatted_content
+        assert formatted_content["documentation_url_path"] == "/docs"
+    else:
+        assert "documentation_url_path" not in formatted_content
