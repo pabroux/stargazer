c084456cf4cfa69149372d7637d4eb855e703c76
pabroux <pierre-alexandrebroux@live.fr>
2025-03-21 10:31:55 +0100

    style: change format for method calls.

--- apps/auth/utils.py
+++ apps/auth/utils.py
@@ -89,7 +89,9 @@ def create_access_token(
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
-        to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM,
     )
     return encoded_jwt
 
@@ -119,7 +121,9 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> Use
     )
     try:
         payload = jwt.decode(
-            token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM]
+            token,
+            settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHM],
         )
         username = payload.get("sub")
         if username is None:
