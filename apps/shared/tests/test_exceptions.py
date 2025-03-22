8b1ae07d9000a137a0ee5aba23885b07d14dd9a9
pabroux <pierre-alexandrebroux@live.fr>
2025-03-22 14:09:54 +0100

    test(shared): reinforce tests for exceptions.

--- apps/shared/tests/test_exceptions.py
+++ apps/shared/tests/test_exceptions.py
@@ -7,33 +7,46 @@ import pytest
 from fastapi import status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from fastapi.testclient import TestClient
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.requests import Request
 
 from apps.shared.exceptions import http_exception_handler, validation_exception_handler
-from main import app
-
-client = TestClient(app)
+from apps.shared.utils import get_formatted_content
 
 
 @pytest.mark.anyio
 async def test_http_exception_handler() -> None:
     """Tests the `http_exception_handler` function.
 
-    Testst that the response object is an instance of `JSONResponse`.
+    Tests that the response object is an instance of `JSONResponse`.
     """
-    exc = StarletteHTTPException(status.HTTP_200_OK)
+    exc = StarletteHTTPException(status_code=status.HTTP_200_OK, detail="detail")
     response = await http_exception_handler(Request({"type": "http"}), exc)
     assert isinstance(response, JSONResponse)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.body == response.render(
+        get_formatted_content(
+            str(exc.detail),
+            exc.status_code,
+        ),
+    )
 
 
 @pytest.mark.anyio
 async def test_validation_exception_handler() -> None:
     """Tests the `validation_exception_handlerr` function.
 
-    Testst that the response object is an instance of `JSONResponse`.
+    Tests that the response object is an instance of `JSONResponse`, has
+    a 422 Unprocessable Entity status code and the correct content.
     """
     exc = RequestValidationError(errors="errors")
     response = await validation_exception_handler(Request({"type": "http"}), exc)
     assert isinstance(response, JSONResponse)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response.body == response.render(
+        get_formatted_content(
+            "Invalid input",
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            exc.errors(),
+        ),
+    )
